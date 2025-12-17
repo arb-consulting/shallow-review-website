15748011186008a8401d2588df1d7fffd6727345
Tomas Gavenciak <gavento@gmail.com>
2025-12-17 22:18:24 +0100

    Minor update: subtitle

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,7 +1,7 @@
 export const APP_TITLE = "Shallow Review of Technical AI Safety, 2025";
 export const APP_SHORT_TITLE = "Shallow Review 2025";
 export const APP_DESCRIPTION =
-  "A map of technical AI safety: 800+ papers in 80 research agendas to help you navigate";
-export const APP_AUTHORS = "technicalities, gavento, et al.";
+  "A shallow-by-design review of technical AI safety research in 2025: 800+ papers and posts across 80+ research agendas.";
+export const APP_AUTHORS = "Gavin Leech, Tomáš Gavenčiak, Stephen McAleese, Peli Grietzer, Stag Lynn, jordine, Ozzie Gooen, Violet Hour, ramennaut, et al.";
 export const GITHUB_REPO_URL = "https://github.com/quantified-uncertainty/shallow-review-website";
 export const GITHUB_DATA_URL = "https://github.com/quantified-uncertainty/shallow-review-website/tree/main/src/data";
